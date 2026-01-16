5a703f7ab39d00eee0d730139d33d2ca2113765e
Farmaan Mantoo <farmaan@IdeaPad>
2026-01-16 22:22:50 +0530

    changes

--- i3/.config/i3/display.sh
+++ i3/.config/i3/display.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-EXT_MON="HDMI-1"
+EXT_MON="HDMI-0"
 
 check_mon() {
     xrandr | grep "$EXT_MON connected" > /dev/null
 }
 
 set_mon() {
-    xrandr --output "$EXT_MON" --auto --right-of eDP-1
+    xrandr --output "$EXT_MON" --auto --right-of DP-0
 }
 
 set_auto() {
